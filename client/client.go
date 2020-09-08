15f6be989315b3f447eabce53b404e2d60f3283a
justlittledream <1074726545@qq.com>
2020-09-08 10:28:05 +0800

    first commit

---
+++ client/client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	// ip =  10.190.180.160
+	conn, err := net.Dial("tcp", "10.190.180.160:8888")
+	if err != nil {
+		fmt.Println("client connect failed = ", err)
+	}
+	//fmt.Printf("connect 成功 = %v", conn)
+	//从终端获取输入
+	fmt.Println("连接服务器成功！！！")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("reader err = ", err)
+		}
+		s := strings.Trim(str, "\r\n")
+		_, err = conn.Write([]byte(str))
+		if err != nil {
+			fmt.Println("write err = ", err)
+		}
+		if s == "exit" {
+			fmt.Println("客户端退出")
+			break
+		}
+		//将输入法送给服务器端
+		//fmt.Printf("客户端发送了%d字节的数据\n", n)
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func process(conn net.Conn) {
+
+	defer conn.Close() //关闭
+	for {
+		buf := make([]byte, 1024)
+		//fmt.Printf("服务器在等待%v的输入\n", conn.RemoteAddr().String())
+		n, err := conn.Read(buf)
+		if strings.Trim(string(buf[:n]), "\r\n") == "exit" {
+			fmt.Println("客户端退出,等待新的客户连接")
+			return
+		}
+		if n == 0 || err != nil {
+			fmt.Println("出现不可预料的错误 = ", err)
+			break
+		}
+		fmt.Print(string(buf[:n]))
+	}
+}
+
+func main() {
+	fmt.Println("服务器开始监听……")
+	listen, err := net.Listen("tcp", "10.190.180.160:8888")
+	if err != nil {
+		fmt.Println("listen error = ", err)
+		return
+	}
+	defer listen.Close() //延时关闭
+	fmt.Println("等待客户来连接")
+	//循环等待
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Accept() err = ", err)
+		} else {
+			fmt.Printf("连接成功，Accept() suc = %v 客户端 ip = %v\n", conn, conn.RemoteAddr().String())
+		}
+		go process(conn)
+	}
+	//fmt.Printf("listen suc= %v", listen)
+}
